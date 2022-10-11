906a1bd80ef4a027f073466afefa995b8a25b2a8
Retrontology <retrontology@hotmail.com>
2022-10-11 00:15:19 -0500

    only use pictures with GPS

--- exif2geojson.py
+++ exif2geojson.py
@@ -13,8 +13,9 @@ def main():
     count = 0
     for raw in raws:
         data = exiflib.get_exif(raw)
-        snaps.append(data)
-        count = count + 1
+        if 'GPSLatitude' in data['GPSInfo']:
+            snaps.append(data)
+            count = count + 1
     with open(OUTFILE, 'w') as outfile:
         outfile.write(geojsonlib.create_geojson(snaps))
 
