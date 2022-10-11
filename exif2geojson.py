d8e387a5494b457c4b33e1a4a04169e1958344a3
Retrontology <retrontology@hotmail.com>
2022-10-11 00:17:49 -0500

    simple logging

--- exif2geojson.py
+++ exif2geojson.py
@@ -14,10 +14,15 @@ def main():
     for raw in raws:
         data = exiflib.get_exif(raw)
         if 'GPSLatitude' in data['GPSInfo']:
+            print(f'Appended {raw}')
             snaps.append(data)
             count = count + 1
+        else:
+            print(f'No GPS info in {raw}, discarding...')
+    print(f'Creating geojson with {count} markers')
     with open(OUTFILE, 'w') as outfile:
         outfile.write(geojsonlib.create_geojson(snaps))
+    print(f'geojson written to {OUTFILE}')
 
 if __name__ == "__main__":
     main()
